fc671d6d300af500390761af8ea335cf5db19b83
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2018-02-24 16:22:02 +0900

    Initial commit with a hello-world server

---
+++ web.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'hello, world'
+
+if __name__ == '__main__':
+  app.run()
