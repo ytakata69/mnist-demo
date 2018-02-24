8e2024dab8b27eeec42b15e1ba08ab435be72e00
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2018-02-24 19:40:49 +0900

    Add the image uploading function

--- web.py
+++ web.py
@@ -1,13 +1,48 @@
 # -*- coding: utf-8 -*-
 
+# References:
+#   A fileupload app with flask
+#   https://github.com/arvelt/flask-fileupload-sample
+#   Flaskで画像アップローダー
+#   https://qiita.com/Gen6/items/f1636be0fe479f42b3ee
+
 import os
-from flask import Flask
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+from werkzeug import secure_filename
+from PIL import Image
+
+ALLOWED_EXTENSIONS = set(['PNG', 'png', 'JPG', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 
-@app.route('/')
+def allowed_file(filename):
+  return '.' in filename and \
+         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET'])
 def index():
-  return 'hello, world'
+  return render_template('index.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+  if request.method == 'POST':
+    file = request.files['img_file']
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      img = Image.open(file)
+      result = {
+        "Result": {
+          "filename": filename,
+          "bands": img.getbands(),
+          "bbox":  img.getbbox()
+        }
+      }
+      return jsonify(ResultSet=result)
+
+    else:
+      return ''' <p>許可されていない拡張子です</p> '''
+  else:
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
